aff88be820299fb1c91dfd275a2fe5dfcbc376b2
EduardMaster <eduardkiller@hotmail.com>
2023-08-18 15:29:49 -0300

    css classname h-screen insted of direct css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Home() {
 
   return (
-    <main className="flex p-4 bg-gray-800" style={{ height: "100vh" }}>
+    <main className="flex p-4 bg-gray-800 h-screen" >
       <PathEditor />
     </main>
   )
