7ba04b908114bda4faf65ad0f05c00dc28f3443d
EduardMaster <eduardkiller@hotmail.com>
2023-08-17 21:07:58 -0300

    PathEditor app for Linux or WSL

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import PathEditor from '@/components/PathEditor'
+import Image from 'next/image'
+
+export default function Home() {
+
+  return (
+    <main className="w-full flex justify-center items-center ">
+      <PathEditor />
+    </main>
+  )
+}
